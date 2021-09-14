a61cb0bbd94961ce292f5722a4f3077ae2da6361
Lorenzo Gatti <l.gatti@utwente.nl>
2021-09-14 17:06:08 +0200

    Added Demo Server
    still somewhat buggy

--- PASS.py
+++ PASS.py
@@ -44,7 +44,7 @@ currentpath = os.getcwd()
     #main(file, 'y')
 
 #main(os.path.dirname(currentpath) + '/Europa_League/NewInfoXMLs/rea_aja_5032019.xml', 'y')
-main(currentpath + '/JSONGameData/2goals_1scorer_succession.json', 'y')
+main(currentpath + '/JSONGameData/ita_oos_20210626.json', 'y')
 
 #if __name__ == '__main__':
     #main(sys.argv[1:])
